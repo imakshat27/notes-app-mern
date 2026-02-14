034de4c2a14b8ba1f1f767c5d69bcdd537a05100
Akshat <agarwalakshat2710@gmail.com>
2026-02-14 14:41:04 +0530

    update axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "/api";
-
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "/api",
 });
 
 export default api;
